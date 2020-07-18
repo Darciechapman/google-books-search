f373c31073ebe44d6b302796efe1c0a3a73d1fab
Darciechapman <darciechapman96@gmail.com>
2020-07-18 11:30:00 +0800

    sytax error

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
-import AppNavbar from './components/Navbar';
+import AppNavbar from './components/NavBar';
 
 import * as API from './utils/API';
 
